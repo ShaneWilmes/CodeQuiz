d81ee4143ff04085fd9bf426468a872fd0662e8f
ShaneWilmes <shanewilmes@yahoo.com>
2022-03-24 18:07:39 -0600

    alterations

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,12 @@ var questionText = document.querySelector(".question-text")
 var questionContainer = document.querySelector(".question-container")
 
 var timer;
-var timerCount = 60;
-var currentQuestionIndex = 0;
-var isWin = false;
+var timerCount;  // Timer is also the scorekeeper
+var currentQuestionIndex = 0;  // Which question are we on?
+var gameOver = false;
+var buttons = [];  // Holds references to our buttons on screen
+var correct = 0;
+var incorrect = 0;
 
 // Array used store questions and answers
 var questions = [
@@ -35,51 +38,108 @@ var questions = [
 ];
 
 function startGame() {
-    isWin = false;
+    gameOver = false;
     startButton.disabled = true;
     timerCount = 60;
+    currentQuestionIndex = 0;
 
-    showQuestion();
-    // Start Timer
+    playGame();
+    startTimer();
 }
 
-var buttons = [];
+function endGame() {
+    quizGame.textContent = "GAME OVER";
+    startButton.disabled = false;
+    playGame();
+}
 
-function showQuestion() {
+function playGame() {
+    if (currentQuestionIndex <= questions.length && gameOver != true) {
+        var currentQuestion = quesions[currentQuestionIndex];
+        setQuestion(currentQuestion.question);
+        showAnswers(currentQuestion);
 
+    } else {
+        resetQuesiton();
+        // get timer value for leaderboard
+        gameOver = true;
+    }
 
+}
+// I don't think 2 if statements are needed
 
-    if (currentQuestionIndex <= questions.length) {
-        var currentQuestion = questions[currentQuestionIndex];
-        questionText.textContent = currentQuestion.question;
-
-
-        
-
-        for (let i = 0; i < currentQuestion.answers.length; i++) {
-            var answer = currentQuestion.answers[i];
-            let button = document.createElement("button");
-            button.innerHTML = answer;
-            button.name = "questionButton";
-            button.addEventListener("click", function() {
-                console.log(answer + " " + currentQuestion.correctAnswer)
-                checkAnswer(answer, currentQuestion.correctAnswer);
-            });
-            buttons.push(button)
-            questionContainer.appendChild(button);
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (gameOver && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                // winGame();
+            }
         }
+        // Tests if time has run out
+
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+}
+
+function setQuestion(currentQuestion) {
+    questionText.textContent = currentQuestion
+}
+
+function showAnswers(currentQuestion) {
+
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+        var answer = currentQuestion.answers[i];
+        let button = document.createElement("button");
+        button.innerHTML = answer;
+        button.name = "questionButton";
+        button.addEventListener("click", function () {
+            console.log(answer + " " + currentQuestion.correctAnswer)
+            checkAnswer(answer, currentQuestion.correctAnswer);
+        });
+        buttons.push(button)
+        questionContainer.appendChild(button);
     }
 }
 
+// Checking answers 
 function checkAnswer(answer, correctAnswer) {
     if (answer === correctAnswer) {
-        for (let i = 0; i < buttons.length; i++) {
-            questionContainer.removeChild(buttons[i]);
-        }
-        buttons = [];
         currentQuestionIndex++;
-        showQuestion();
-    } 
+        resetQuesiton();
+        playGame();
+        correct++
+        correctText.textContent = correct;
+    } else {
+        incorrect++
+        incorrectText.textContent = incorrect;
+    }
+}
+
+function resetQuesiton() {
+    for (let i = 0; i < buttons.length; i++) {
+        questionContainer.removeChild(buttons[i]);
+    }
+
+    setQuestion("");
+    buttons = [];
 }
 
 startButton.addEventListener("click", startGame);
+
+var resetButton = document.querySelector(".reset-button")
+
+function resetGame() {
+
+    correct = 0;
+    incorrect = 0;
+}
